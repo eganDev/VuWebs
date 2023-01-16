de3299f0b130e11251a2cfa49675d7d9597c71a3
eganDev <vuvo070403@gmail.com>
2023-01-16 11:05:58 +0700

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 
 import "../sass/About.scss"
-import "../data/INFO_DATA"
+import INFO_DATA from "../data/INFO_DATA"
+import VuAvt from "../asset/vuavt.jpeg"
+import Aos from 'aos'
 
 export default function About() {
+  Aos.init()
   return (
-    <div className='about'>
+    <div className='about ' id='about' >
         <div className='about-container'>
            <div>
-                <h1>INTRODUCTION</h1>
+                <h1>ABOUT ME</h1>
            </div>
            <div className='about-container-info'>
                 <div className='about-container-info__image'>
                         <div className='about-container-info__image_detail'>
-                            <img src="" alt="" />
+                            <img src={VuAvt} alt="avt" />
+                        </div>
+                        <div className='about-container-info__image_name'>
+                            {/* <h2>Vo Van Vu</h2> */}
                         </div>
                 </div>
-                <div className='about-container-info__content'>
-                        <div className='about-container-info__content__languages'></div>
-                        <div className='about-container-info__content__introduces'></div>
-                </div>
+                {
+                  INFO_DATA.map((value, index) => (
+                    <div className='about-container-info__content' key={index}>
+                      <div className='about-container-info__content__introduces'>
+                        <p>{value.introduce}</p>
+                      </div>
+                    </div>
+                  ))
+                }
            </div>
         </div>
     </div>
