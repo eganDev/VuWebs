e9c46e8e889ea70b18b017f9108afe40fa4a593c
eganDev <vuvo070403@gmail.com>
2023-01-26 02:48:12 +0700

    update-new

--- src/data/PROJECT_DATA.js
+++ src/data/PROJECT_DATA.js
@@ -1,18 +1,32 @@
 const PROJECT_DATA = [
     {
         title: "Lyrics",
-        tech: ["ReactJs", "Taiwindcss", "Redux", "Axios"],
+        tech: ["ReactJs", "Taiwindcss", "Redux", "Axios", "Responsive"],
         desc: "A Shazam based music app made for music enjoyment",
         image: "music",
         link: "#"
     },
     {
-        title: "Note Pad",
+        title: "Note-Pad Taking",
         tech: ["ReactJs", "Taiwindcss"],
         desc:  "A Note App that makes it easy for everyone to take notes",
         image: "note",
         link: "https://egandev.github.io/NOTE-PAD/"
     },
+    {
+        title: "Frog Lentgh Unit",
+        tech: ["ReactJs", "Taiwindcss", "Responsive"],
+        desc: "A web application created to convert units of length, color and promote friendliness.",
+        image: "froglengthunit",
+        link: "https://egandev.github.io/FrogLengthUnit/"
+    },
+    {
+        title: "Macintosh clone",
+        tech: ["HTML", "CSS", "Javascript"],
+        desc: "A website created to consolidate knowledge and learn more about front-end. Simulate an application store of a Macintosh computer.",
+        image: "macintosh",
+        link: "https://egandev.github.io/Vu_clone_Macintosh/",
+    }
 ]
 
 export default PROJECT_DATA
