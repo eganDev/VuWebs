a60e4684c777d454c5d4552310789a54330c342e
vuvudev <vuvo070403@gmail.com>
2023-01-26 17:34:08 +0700

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import "../sass/Contact.scss"
-import { MdLocationOn, MdEmail } from "react-icons/md";
+import { MdLocationOn, MdEmail, MdOutgoingMail } from "react-icons/md";
 import { BsFillTelephoneFill } from "react-icons/bs";
+import Aos from 'aos';
 
 const Contact = () => {
+    Aos.init();
     return (
         <div className='contact'>
             <div className='contact-container'>
@@ -12,7 +14,7 @@ const Contact = () => {
                     <h1>CONTACT</h1>
                 </div>
                 <div className='contact-info'>
-                    <div className='contact-info__box'>
+                    <div className='contact-info__box' data-aos="fade-up">
                         <div className='contact-info__box__info'>
                             <MdLocationOn className='icon'></MdLocationOn>
                             <p>Hoa Hai, Da Nang, Viet Nam</p>
@@ -26,14 +28,22 @@ const Contact = () => {
                             <p>+84356611341</p>
                         </div>
                     </div>
-                    <div className='contact-info__connect'>
+                    <div className='contact-info__connect'  data-aos="fade-up">
                         <div className='contact-info__boxc'>
                             <h3>Mail to me?</h3>
-                            <p>If my experience can contribute to your business, email is the best medium to communicate with me</p>
-                            <button>Send Email</button>
+                            <p>If my experience can contribute to your business, email is the best medium to communicate with me!</p>
+                            <button>
+                                <MdOutgoingMail className='icon-1'></MdOutgoingMail>
+                                Send Email
+                            </button>
                         </div>
                         <div className='contact-info__boxc'>
-                       
+                            <h3>Talk to me?</h3>
+                            <p>To easily find out more information about me, as well as exchange jobs. Call and talk to me about your business!</p>
+                            <button>
+                                <MdOutgoingMail className='icon-1'></MdOutgoingMail>
+                                Go to Phone
+                            </button>
                         </div>
                     </div>
                 </div>
