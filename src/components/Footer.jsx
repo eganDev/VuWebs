318bc45b6e43f81a0cf37c1450283ab627c5ed37
VuVuDev <vuvo070403@gmail.com>
2023-08-17 02:24:06 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -45,7 +45,7 @@ export default function Footer() {
                         <p>Gmail</p>
                     </div>
                     <div className='footer__contact__items'
-                    onClick = {() => window.open("https://github.com/VuVuDev", "_blank")}>
+                    onClick = {() => window.open("https://github.com/vu-sudo", "_blank")}>
                         <BsGithub className='icon-footer'></BsGithub>
                         <p>Github</p>
                     </div>
